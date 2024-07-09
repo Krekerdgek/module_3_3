28b35bb1db14e8201680e56195e90205dd86836d
Krekerdgek <dimos-79@mail.ru>
2024-07-09 10:51:37 +0300

    Update module_3_3_corrected.py

--- module_3_3.py
+++ module_3_3.py
@@ -1,14 +1,13 @@
 # Создаём функцию print_params
-def print_params(a=1, b='строка', c=True):
+def print_params(a = 1, b = 'строка', c = True):
     print(a, b, c)
-    # вызов фу-ции print parms
-    print_params(b = 25)
-    print_params(c = [1, 2, 3])
-    # распаковка параметров
-    values_list = ['Дмитрий', 999, [1, 2, 3]]
-    values_dict = {'a': [1, 2, 3], 'b': 'Дмитрий', 'c': 999}
-    print_params(*values_list)
-    print_params(**values_dict)
-    # Распаковка + отдельные параметры:
-    values_list_2 = ['абв', [4, 5, 6]]
-    print_params(*values_list_2, 42)
+print_params(b = 25)
+print_params(c = [1,2,3])
+# распаковка параметров
+values_list = ['Дмитрий', 999, [1, 2, 3]]
+values_dict = {'a': [1, 2, 3], 'b': 'Дмитрий', 'c': 999}
+print_params(*values_list)
+print_params(**values_dict)
+# Распаковка + отдельные параметры:
+values_list_2 = ['абв', [4, 5, 6]]
+print_params(*values_list_2, 42)
